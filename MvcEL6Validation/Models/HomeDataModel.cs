26b21c95ddec77e794a8cf3748702f91bcc67b45
damienbod <damien_bod@hotmail.com>
2013-07-11 10:27:32 +0200

    custom validation dependency items

--- MvcEL6Validation/Models/HomeDataModel.cs
+++ MvcEL6Validation/Models/HomeDataModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using MvcEL6Validation.Validators;
 
 namespace MvcStandardValidation.Models
 {
+    public class Enums
+    {
+        public enum ApplicationStatus
+        {
+            Pending = 0,
+            Submitted = 1,
+            Disapproved = 2,
+            Approved = 3
+        }
+    }
+
+
     public class HomeDataModel
     {
         [Required]
@@ -23,10 +36,22 @@ namespace MvcStandardValidation.Models
 
         public double ValDouble1 { get; set; }
 
+        [Range(10.0, 100.0)]
         public double? ValDouble2 { get; set; }
 
         public DateTime ValDateTime1 { get; set; }
 
         public DateTime? ValDateTime2 { get; set; }
+
+        public SelectList ApplicationStatus { get; set; }
+
+        [Required]
+        public int SelectedApplicationStatus { get; set; }
+
+        [RequiredIf("SelectedApplicationStatus",
+            (int)Enums.ApplicationStatus.Submitted,
+            (int)Enums.ApplicationStatus.Approved,
+            (int)Enums.ApplicationStatus.Disapproved, ErrorMessage = "The Application Submission Date is required")]
+        public DateTime? ApplicationSubmitted { get; set; } 
     }
 }
